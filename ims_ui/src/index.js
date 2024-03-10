9e39f78e17c84e43507391d1fc04811d804e14d4
akomarzx <62472112+akomarzx@users.noreply.github.com>
2024-03-10 18:16:51 -0400

    Task/Deployment Complete(#5)
    * Updated deployment
    
    * added build step
    
    * Update Client

--- ims_ui/src/index.js
+++ ims_ui/src/index.js
@@ -8,7 +8,7 @@ import { AuthProvider } from "react-oidc-context";
 const oidcConfig = {
   authority: "https://ronaldjro.dev/auth/realms/IMS-Dev/",
   client_id: "664148af-85bf-4ae2-9143-32920b31a06e-IMS-Dev",
-  redirect_uri: "http://localhost:3000/*",
+  redirect_uri: "https://ronaldjro.dev/*",
   client_secret: "8jdGrGQFgHG1OVSD63Teiy6YPIkHxJnj"
 };
 
