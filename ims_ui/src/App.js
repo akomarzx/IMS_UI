cf1ac1b48dc734dc5268eb1386890ff23618292a
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2024-03-10 19:29:32 -0400

    IMS-22 Trial Testing

--- ims_ui/src/App.js
+++ ims_ui/src/App.js
@@ -4,33 +4,39 @@ import { useAuth } from "react-oidc-context";
 
 
 function App() {
-  const auth = useAuth();
+    const auth = useAuth();
 
-  switch (auth.activeNavigator) {
-      case "signinSilent":
-          return <div>Signing you in...</div>;
-      case "signoutRedirect":
-          return <div>Signing you out...</div>;
-  }
+    switch (auth.activeNavigator) {
+        case "signinSilent":
+            return <div>Signing you in...</div>;
+        case "signoutRedirect":
+            return <div>Signing you out...</div>;
+    }
 
-  if (auth.isLoading) {
-      return <div>Loading...</div>;
-  }
+    if (auth.isLoading) {
+        return <div>Loading...</div>;
+    }
 
-  if (auth.error) {
-      return <div>Oops... {auth.error.message}</div>;
-  }
+    if (auth.error) {
+        return <div>Oops... {auth.error.message}</div>;
+    }
 
-  if (auth.isAuthenticated) {
-      return (
-      <div>
-          Hello {auth.user?.profile.sub}{" "}
-          <button onClick={() => void auth.removeUser()}>Log out</button>
-      </div>
-      );
-  }
+    if (auth.isAuthenticated) {
+        return (
+            <div>
+                Hello {auth.user?.profile.sub}{" "}
+                <button onClick={() => void auth.removeUser()}>Log out</button>
+            </div>
+        );
+    }
 
-  return <button onClick={() => void auth.signinRedirect()}>Log in</button>;
+    return (
+        <>
+            <h1>IMS UI</h1>
+            <button onClick={() => void auth.signinRedirect()}>Log in</button>
+        </>
+    );
+
 }
 
 export default App;
